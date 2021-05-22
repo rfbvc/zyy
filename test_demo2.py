3a5603d274656aca9117072345e787adc351ade6
rfbvc <82200676+rfbvc@users.noreply.github.com>
2021-05-22 17:14:15 +0800

    Delete test_demo2.py

--- test_demo2.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import yaml
-def add_function(a,b):
-    return a+b
-@pytest.mark.parametrize("a,b,expected",
-                        yaml.safe_load(open("./data.yml"))["datas"],
-                        ids=yaml.safe_load(open("./data.yml"))["myid"])
-def test_add(a,b,expected):
-    assert add_function(a,b) == expected
